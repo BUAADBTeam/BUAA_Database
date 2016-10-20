72a9fbe1c87d29c8c68d1017bcef991962a8ceae
Kira0096 <704788525@qq.com>
2016-10-20 20:44:45 +0800

    调试购物车完成

--- application/models/Cartm.php
+++ application/models/Cartm.php
@@ -11,21 +11,26 @@ class Cartm extends Model {
 	function getCart($userid)
 	{
 		
+		try {
+			$sql = "SELECT * FROM orderitems
+					  WHERE orderid in (SELECT orderid FROM orders 
+	                  WHERE userid = :userid AND status < 2)"; 
+					  
+			$pdo = $this->db->connect();
+	        $sth = $pdo->prepare($sql);
+	        $sth->bindValue(':userid', $userid);
+	        $sth->execute();
+	        $cart = array();
+	        while($row = $sth->fetch(PDO::FETCH_ASSOC)) {
+	        	$cart[] = $row;
+	        }
 
-		$sql = "SELECT * FROM orderitems
-				  WHERE orderid in (SELECT orderid FROM orders 
-                  WHERE userid = ". $userid. "AND status < 2;)"; 
-				  
-		$pdo = $this->db->connect();
-        $sth = $pdo->prepare($sql);
-        $sth->excute();
-        $cart = array();
-        while($row = $sth->fetch(PDO::FETCH_ASSOC)) {
-        	$cart[] = $row;
-        }
-
-        $pdo = null;
-        return $cart;
+	        $pdo = null;
+	        return $cart;
+	    } catch(PDOException $e) {
+	    	$pdo = null;
+	    	return null;
+	    }
 	}
 
 	function addFood($userid, $itemid, $amount)
@@ -34,35 +39,63 @@ class Cartm extends Model {
 			return False;
 		try {
 			$pdo = $this->db->connect();
+
+			$sql = "SELECT price 
+	        		FROM products
+	        		WHERE id = $itemid";
+	        $sth = $pdo->prepare($sql);
+	        $sth->execute();
+
+			if($result = $sth->fetch(PDO::FETCH_ASSOC))
+            	$price = $result['price'];	        
+            else 
+            	return False;
+
+            $price *= $amount;
+
 			$sql = "SELECT orderid FROM orders 
                   WHERE userid = :userid AND status < 2"; 
             $sth = $pdo->prepare($sql);
             $sth->bindValue(':userid', $userid);
-            $sth->excute();
-            $result = $sth->fetch(PDO::FETCH_ASSOC);
-            $orderid = $result['orderid'];
+            $sth->execute();
+            if($result = $sth->fetch(PDO::FETCH_ASSOC))
+            	$orderid = $result['orderid'];
+            else
+            	return False;
 
-			$sql = "INSERT INTO orderitems(userid, orderid, itemid, amount) 
-					  VALUES(:userid, :orderid, :itemid, :amount)"; 
-					  
-			
-	        $sth = $pdo->prepare($sql);
+            $sql = "SELECT COUNT(*) 
+            		FROM orderitems
+            		WHERE userid = :userid  AND orderid = :orderid AND itemid = :itemid"; 
+            $sth = $pdo->prepare($sql);
 	        $sth->bindValue(':userid', $userid);
 	        $sth->bindValue(':orderid', $orderid);
 	        $sth->bindValue(':itemid', $itemid);
-	        $sth->bindValue(':amount', $amount);
-	        $sth->excute();
+	        $sth->execute();
+	        $result = $sth->fetch();
+	        // var_dump($result);
+	        if($result[0] > 0) {
+	        	$sql = "UPDATE orderitems
+	        			SET amount = amount + $amount
+            			WHERE userid = :userid  AND orderid = :orderid AND itemid = :itemid";
 
-	        $sql = "SELECT price 
-	        		FROM product
-	        		WHERE id = $itemid";
-	        $sth = $pdo->prepare($sql);
-	        $sth->excute();
-			$result = $sth->fetch(PDO::FETCH_ASSOC);
-            $price = $result['price'];	        
-
-            $price *= $amount;
-
+	            $sth = $pdo->prepare($sql);
+		        $sth->bindValue(':userid', $userid);
+		        $sth->bindValue(':orderid', $orderid);
+		        $sth->bindValue(':itemid', $itemid);
+		        $sth->execute();
+	        }
+	        else {
+				$sql = "INSERT INTO orderitems(userid, orderid, itemid, amount) 
+						  VALUES(:userid, :orderid, :itemid, :amount)"; 	
+		        $sth = $pdo->prepare($sql);
+		        $sth->bindValue(':userid', $userid);
+		        $sth->bindValue(':orderid', $orderid);
+		        $sth->bindValue(':itemid', $itemid);
+		        $sth->bindValue(':amount', $amount);
+		        $sth->execute();
+		    }
+		    
+	        
             $sql = "UPDATE orders
             		SET total = total + $price
             		WHERE orderid = $orderid";
@@ -80,42 +113,64 @@ class Cartm extends Model {
 	{
 		if(!is_numeric($amount) || !($amount + 0 > 0)) 
 			return False;
-
+		$amount = $amount + 0;
 		try {
 					  
 			$pdo = $this->db->connect();
+
+			$sql = "SELECT price 
+	        		FROM products
+	        		WHERE id = $itemid";
+	        $sth = $pdo->prepare($sql);
+	        $sth->execute();
+
+			if($result = $sth->fetch(PDO::FETCH_ASSOC))
+            	$price = $result['price'];	        
+            else 
+            	return False;
+            $price *= $amount;
 
 			$sql = "SELECT orderid FROM orders 
                   WHERE userid = :userid AND status < 2"; 
             $sth = $pdo->prepare($sql);
             $sth->bindValue(':userid', $userid);
-            $sth->excute();
-            $result = $sth->fetch(PDO::FETCH_ASSOC);
-            $orderid = $result['orderid'];
+            $sth->execute();
+            if($result = $sth->fetch(PDO::FETCH_ASSOC))
+            	$orderid = $result['orderid'];
+            else
+            	return False;
 
 	        $sql = "SELECT amount 
 	        		FROM orderitems
 					WHERE orderid = $orderid AND itemid = :itemid"; 
 	        $sth = $pdo->prepare($sql);
 	        $sth->bindValue(':itemid', $itemid);
-	        $sth->excute();
+	        $sth->execute();
 	        $result = $sth->fetch();
-
+	        
 	        if($result[0] > $amount) {
 	        	$sql = "UPDATE orderitems
 	        			SET amount = amount - $amount
 						WHERE orderid = $orderid AND itemid = :itemid"; 
 				$sth = $pdo->prepare($sql);
 	        	$sth->bindValue(':itemid', $itemid);
-	        	$sth->excute();
+	        	$sth->execute();
 	        }
-	        else{
-	        	$sql = "DELETE orderitems
+	        else if($result[0] == $amount){
+
+	        	$sql = "DELETE FROM orderitems
 						WHERE orderid = $orderid AND itemid = :itemid"; 
 				$sth = $pdo->prepare($sql);
 	        	$sth->bindValue(':itemid', $itemid);
-	        	$sth->excute();
+	        	$sth->execute();
 	        }
+	        else
+	        	return False;
+
+	        $sql = "UPDATE orders
+            		SET total = total - $price
+            		WHERE orderid = $orderid";
+            $pdo->exec($sql);
 	        $pdo = null;
 	    } catch(PDOException $e) {
 	    	return False;
