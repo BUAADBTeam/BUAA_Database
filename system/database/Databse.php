cdcf6ddf806a1d5160481fbc317b90eb4799e674
Kira0096 <704788525@qq.com>
2016-10-30 01:43:16 +0800

    修改了Database中的参数

--- system/database/Databse.php
+++ system/database/Databse.php
@@ -36,16 +36,13 @@ class Database {
 
 	public function prepare($sql) {
 		$this->statement = $this->pdo->prepare($sql);
-		$this -> SqlBug .= "\n". '<!--DebugSql: ' . $sql . '-->' . "\n";
+		// $this -> SqlBug .= "\n". '<!--DebugSql: ' . $sql . '-->' . "\n";
 	}
 	
-	public function bindParam($parameter, $variable, $data_type = PDO::PARAM_STR, $length = 0) {
-		if ($length) {
-			$this->statement->bindParam($parameter, $variable, $data_type, $length);
-		} 
-		else {
-				$this->statement->bindParam($parameter, $variable, $data_type);
-		}
+	public function bindParam($parameter, $variable) {
+		
+		$this->statement->bindParam($parameter, $variable);
+		
 	}
 	
 	public function execute($mode = '') {
@@ -56,10 +53,10 @@ class Database {
 				while (!empty($mode) && $row = $this->statement->fetch()) {
 					$data[] = $row;
 				}
-				$result = new stdClass();
-				$result->row = (isset($data[0])) ? $data[0] : array();
-				$result->rows = $data;
-				$result->num_rows = $this->statement->rowCount();
+				$result = array();
+				$result['row'] = (isset($data[0])) ? $data[0] : array();
+				$result['rows'] = $data;
+				$result['num_rows'] = $this->statement->rowCount();
 			}
 		} 
 		catch(PDOException $e) {
@@ -70,10 +67,10 @@ class Database {
 			return $result;
 		}
 		else {
-			$result = new stdClass();
-			$result->row = array();
-			$result->rows = array();
-			$result->num_rows = 0;
+			$result = array();
+			$result['row'] = array();
+			$result['rows'] = array();
+			$result['num_rows'] = 0;
 			return $result;
 		}
 	}
@@ -90,10 +87,10 @@ class Database {
 				while ($type == 'SELECT' && $row = $this->statement->fetch()) {
 					$data[] = $row;
 				}
-				$result = new \stdClass();
-				$result->row = (isset($data[0]) ? $data[0] : array());
-				$result->rows = $data;
-				$result->num_rows = $this->statement->rowCount();
+				$result = array();
+				$result['row'] = (isset($data[0]) ? $data[0] : array());
+				$result['rows'] = $data;
+				$result['num_rows'] = $this->statement->rowCount();
 			}
 		} catch (PDOException $e) {
 			trigger_error('Error: ' . $e->getMessage() . ' Error Code : ' . $e->getCode() . ' <br />' . $sql);
@@ -104,10 +101,10 @@ class Database {
 			return $result;
 		} 
 		else {
-			$result = new stdClass();
-			$result->row = array();
-			$result->rows = array();
-			$result->num_rows = 0;
+			$result = array();
+			$result['row'] = array();
+			$result['rows'] = array();
+			$result['num_rows'] = 0;
 			return $result;
 		}
 	}
@@ -119,38 +116,38 @@ class Database {
 	/**
 	* 获得所有查询条件的值
 	*/
-	public function fetchAll($sql, $params = array()) {
-		$rows = $this->query($sql, $params)->rows;
-		return !empty($rows) ? $rows : false;
-	}
+	// public function fetchAll($sql, $params = array()) {
+	// 	$rows = $this->query($sql, $params)->rows;
+	// 	return !empty($rows) ? $rows : false;
+	// }
 	
-	/**
-	* 获得单行记录的值
-	*/
-	public function fetchAssoc($sql, $params = array()) {
-		$row = $this->query($sql, $params)->row;
-		return !empty($row) ? $row : false;
-	}
+	// *
+	// * 获得单行记录的值
 	
-	/**
-	* 获得单个字段的值
-	*/
-	public function fetchColumn($sql, $params = array()) {
-		$data = $this->query($sql, $params)->row;
-		if(is_array($data)) {
-		foreach ($data as $value) {
-			return $value;
-		}
-	}
-	return false;
-	}
+	// public function fetchAssoc($sql, $params = array()) {
+	// 	$row = $this->query($sql, $params)->row;
+	// 	return !empty($row) ? $row : false;
+	// }
+	
+	// *
+	// * 获得单个字段的值
+	
+	// public function fetchColumn($sql, $params = array()) {
+	// 	$data = $this->query($sql, $params)->row;
+	// 	if(is_array($data)) {
+	// 	foreach ($data as $value) {
+	// 		return $value;
+	// 	}
+	// }
+	// return false;
+	// }
 	
 	/**
 	* 返回statement记录集的行数
 	*/
-	public function rowCount($sql, $params = array()) {
-		return $this->query($sql, $params)->num_rows;
-	}
+	// public function rowCount($sql, $params = array()) {
+	// 	return $this->query($sql, $params)->num_rows;
+	// }
 	
 	public function select($data, $table, $where = '', $params = array()) {
 		if(empty($where)) {
@@ -167,44 +164,33 @@ class Database {
 		return $this->query($sql, $params);
 	}
 
-	/**
-	* 插入数据
-	* @param string $table 表名
-	* @param Array $data 数据
-	* @return int InsertId 新增ID
-	*/
-	public function insert($table, $data, $params = array()) {
-		if (!is_array($data) || count($data) == 0) {
+
+	public function insert($table, $column, $params = array()) {
+		if (!is_array($column) || count($column) == 0) {
 			return 0;
 		}
 		$field_arr = array();
 		$value_arr = array();
-		foreach ($data as $key=>$val) {
+		foreach ($column as $key=>$val) {
 			$field_arr[] = "$key ";
 			$value_arr[] = "$val ";
 		}
 		$sql = "INSERT INTO " . $table . "(" . implode(',', $field_arr);
 		$sql .= (") ". "VALUES(". implode(',', $value_arr). ")");
 		$this -> query($sql, $params, $type = 1);
-		return $this->getLastId();
+		return $this->pdo->lastInsertId();
 	}
 	
-	/**
-	* 更新数据
-	* @param string $table 表名
-	* @param Array $data 数据
-	* @param string $where 更新条件
-	* @return int 影响数
-	*/
-	public function update($table, $data, $where = '', $params = array()) {
+	
+	public function update($table, $column, $where = '', $params = array()) {
 		if(empty($where)) {
 			return 0;
 		}
-		if (!is_array($data) || count($data) == 0) {
+		if (!is_array($column) || count($column) == 0) {
 			return 0;
 		}
 		$field_arr = array();
-		foreach ($data as $key=>$val) {
+		foreach ($column as $key=>$val) {
 			$field_arr[] = " `$key` = $val ";
 		}
 		$sql = "UPDATE " . $table . " SET " . implode(',', $field_arr) . " WHERE " . $where;
@@ -225,46 +211,46 @@ class Database {
 	/**
 	* 获得影响集合中
 	*/
-	public function countAffected() {
-		if ($this->statement) {
-			return $this->statement->rowCount();
-		} else {
-			return 0;
-		}
-	}
+	// public function countAffected() {
+	// 	if ($this->statement) {
+	// 		return $this->statement->rowCount();
+	// 	} else {
+	// 		return 0;
+	// 	}
+	// }
 	
-	/*
-	* 获得插入id
-	*/
-	public function getLastId() {
-		return $this->pdo->lastInsertId();
-	}
+	// /*
+	// * 获得插入id
+	// */
+	// public function getLastId() {
+	// 	return $this->pdo->lastInsertId();
+	// }
 	
-	public function escape($value) {
-		$search = array("\\", "\0", "\n", "\r", "\x1a", "'", '"');
-		$replace = array("\\\\", "\\0", "\\n", "\\r", "\Z", "\'", '\"');
-		return str_replace($search, $replace, $value);
-	}
+	// public function escape($value) {
+	// 	$search = array("\\", "\0", "\n", "\r", "\x1a", "'", '"');
+	// 	$replace = array("\\\\", "\\0", "\\n", "\\r", "\Z", "\'", '\"');
+	// 	return str_replace($search, $replace, $value);
+	// }
 	
-	/**
-	* 返回错误信息也包括错误号
-	*/
-	public function errorInfo() {
-		return $this->statement->errorInfo();
-	}
+	// /**
+	// * 返回错误信息也包括错误号
+	// */
+	// public function errorInfo() {
+	// 	return $this->statement->errorInfo();
+	// }
 	
-	/**
-	* 返回错误号
-	*/
-	public function errorCode() {
-		return $this->statement->errorCode();
-	}
+	// /**
+	// * 返回错误号
+	// */
+	// public function errorCode() {
+	// 	return $this->statement->errorCode();
+	// }
 
 	public function close() {
 		$this->pdo = null;
 	}
 
-	public function __destruct() {
-		$this->pdo = null;
-	}
+	// public function __destruct() {
+	// 	$this->pdo = null;
+	// }
 }
