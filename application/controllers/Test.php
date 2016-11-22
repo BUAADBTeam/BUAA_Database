52ee8fa71e1656c858829745afbc5f88d29763db
Kira0096 <704788525@qq.com>
2016-11-22 19:47:53 +0800

    修改了订单处理方式

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -17,18 +17,19 @@ class Test extends Controller {
 	{
 		// $res = $this->orderm->getSpecificOrders(43, userMode);
 		// print_r($res);
-		echo '    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">  
-    <html xmlns="http://www.w3.org/1999/xhtml">  
-    <head>  
-        <title></title>  
-    </head>  
-    <body>  
-        <form action="test/upload" method="post" enctype="multipart/form-data">  
-        选择要上传的图片：<input type="file" name="fileUp" />  
-        <input type="submit" value="上传" />  
-    </form>  
-    </body>  
-    </html>  ';
+		// echo '    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">  
+  //   <html xmlns="http://www.w3.org/1999/xhtml">  
+  //   <head>  
+  //       <title></title>  
+  //   </head>  
+  //   <body>  
+  //       <form action="test/upload" method="post" enctype="multipart/form-data">  
+  //       选择要上传的图片：<input type="file" name="fileUp" />  
+  //       <input type="submit" value="上传" />  
+  //   </form>  
+  //   </body>  
+  //   </html>  ';
+		echo "<img src='http://qr.liantu.com/api.php?&w=200&text=http://baidu.com'>";
 	}
 	public function post()
 	{
